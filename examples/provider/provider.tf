9393ba6b88711bb02b529e28960d4d704fa2da99
hongchaodeng <hongchaodeng1@gmail.com>
2026-01-06 10:13:10 -0800

    fix provider url

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     anyscale = {
-      source  = "github.com/brent/anyscale"
+      source  = "github.com/anyscale/terraform-provider-anyscale"
       version = "~> 0.1"
     }
   }
